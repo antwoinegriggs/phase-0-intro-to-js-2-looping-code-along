2f0decf91c3e83ccf9b9e762a955d45e569d2c28
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-03-01 00:19:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+  const newArray = [];
+  for (let index = 0; index < name.length; index += 1) {
+    const message = `Thank you, ${name[index]}, for the wonderful ${event} gift!`;
+    newArray.push(message);
+  }
+  return newArray;
+}
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n -= 1;
+  }
+}
